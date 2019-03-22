e706fb0ff61ed34fddaf38f26fa134689e6b61e1
Brian Hooper <brian_hooper@msn.com>
2019-03-21 19:41:02 -0700

    Modified travis yml to use correct main

---
+++ CMakeFiles/ci_linked.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ci_linked.dir/main.cpp.o"
+  "ci_linked.pdb"
+  "ci_linked"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ci_linked.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
